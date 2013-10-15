47232325c81518be6a76831007ba0fe2c2d3273c
TeamRedFox001@gmail.com <TeamRedFox001@gmail.com@d51f1277-e13c-1e94-9e0f-bf8cd553c27b>
2013-10-15 21:10:59 +0000

    some baro experiments not ready yet

--- minLOGGER/Vars.h
+++ minLOGGER/Vars.h
@@ -5,21 +5,19 @@
 
 static uint8_t      log_got_home = 0;               // home found
 static long         log_got_home_millis = 0;        // timestamp of got home
+static float        log_home_alt = 0;               // home altitude
 
 static uint8_t      log_fix_type = 0;               // GPS lock
 static uint8_t      log_satellites_visible = 0;     // number of satelites
 static float        log_lat = 0;                    // latitude
 static float        log_lon = 0;                    // longitude
 static float        log_alt = 0;                    // altitude
-static float        log_home_alt = 0;               // home altitude
 static float        log_heading = 0;                // ground course heading
 static float        log_groundspeed = 0;            // ground speed
 static float        log_climb = 0;                  // climb rate
 
 
 #ifdef BAROMETER
-static float        log_baro_temperature = 0;
-static float        log_baro_pressure = 0;
-static float        log_baro_alt = 0;
-static float        log_baro_home_alt = 0;
+//static float        log_temperature = 0;
+static float        log_pressure = 0;
 #endif
